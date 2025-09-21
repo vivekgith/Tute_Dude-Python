8f28475a7a7501f578282f783a0237925f595d93
VIVEK RAJ VERMA <vermavivekraj103@gmail.com>
2025-09-22 04:12:13 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,12 @@
+x=int(input("Enter a number : "))
+if x%2==0:
+    print(x, "is an even number")
+else:
+    print(x, "is an odd number")
+
+i=1
+sum=0
+while i<=50:
+    sum=sum+i
+    i=i+1
+print(sum)
